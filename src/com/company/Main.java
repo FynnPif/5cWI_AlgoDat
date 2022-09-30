b381a7762ecff5dd52bee65f3f91b255d426f6aa
FynnPif <fynn.piffrader@student.htldornbirn.at>
2022-09-30 09:36:06 +0200

    commit Osterformel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("test1");
+
+        Scanner scan = new Scanner(System.in);
+        int J = scan.nextInt();
+        int N = J - 1900;
+        int A = N % 19;
+        int B = (7 * A) / 19;
+        int M = (11 * A + 4 - B) % 29;
+        int Q = N / 4;
+        int W = (N + Q + 31 - M) % 7;
+        int P = 25 - M - W;
+
+        System.out.println("Der naechste Ostersonntag ist am " + P + ".April.");
+
     }
 }
