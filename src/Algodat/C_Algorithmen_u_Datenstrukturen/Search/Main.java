b61b277cf3dd471b5b0638dce9ba3ff227cea107
FynnPif <fynn.piffrader@student.htldornbirn.at>
2022-11-24 09:27:16 +0100

    sequential search refactor

---
+++ src/Algodat/C_Algorithmen_u_Datenstrukturen/Search/Main.java
@@ -0,0 +1,11 @@
+package Algodat.C_Algorithmen_u_Datenstrukturen.Search;
+
+public class Main extends sequential {
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 40, 60, 30, 50, 100};
+        int x = 30;
+
+        sequentialSearchResult(arr, x);
+    }
+}
